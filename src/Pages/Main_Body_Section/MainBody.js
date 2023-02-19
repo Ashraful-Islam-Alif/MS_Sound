0433cb1c6d3f0c2042ecd603c54fe012e5752270
AshrafulIslamAlif <alif.ashrafulislam@gmail.com>
2023-02-19 18:01:32 +0600

    final push

--- src/Pages/Main_Body_Section/MainBody.js
+++ src/Pages/Main_Body_Section/MainBody.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
+import headphone from './headphone.png'
+import frame from './Frame 22.png'
+import rating from './start-icons.png'
 const MainBody = () => {
     return (
-        <div className='grid grid-cols-2 gap-2'>
-            <div className='bg-slate-200 text-6xl'>Alif</div>
-            <div className='bg-slate-200 text-6xl'> Ashraf</div>
+        <div className='grid lg:grid-cols-2 grid-cols-1 gap-4'>
+            <div className='grid grid-cols-2 gap-4'>
+                <div>
+                    <img src={headphone} alt="" srcset="" />
+                </div>
+                <div>
+                    <h2>Beats Studio 3 Wireless Headphone</h2>
+                    <div>
+                        <img src={rating} alt="" srcset="" />
+                        <img src={rating} alt="" srcset="" />
+                        <img src={rating} alt="" srcset="" />
+                        <img src={rating} alt="" srcset="" />
+                        <img src={rating} alt="" srcset="" />
+                    </div>
+                </div>
+            </div>
+            <div>
+                <img src={frame} alt="" srcset="" />
+            </div>
         </div>
     );
 };
